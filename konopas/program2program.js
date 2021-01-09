93ad633891419fc72950b338adb7b88e61e948fb
M. C. DeMarco <mcd@mcdemarco.net>
2021-01-09 18:31:54 -0500

    Handle some nulls.

--- konopas/program2program.js
+++ konopas/program2program.js
@@ -6,10 +6,22 @@ var out = [];
 for (var d=0; d<data.length; d++) {
 	var datum = data[d];
 	//Almost identical to the grenadine format, but a slot in loc: is filled in incorrectly.
-	var result = {"id": datum.id.toString(),
+	if (!datum.hasOwnProperty("loc"))
+		console.log("Item " + datum.id.toString() + ", " + datum.title + " missing location.");
+	else if (!datum.hasOwnProperty("time") || datum.time == null)
+		console.log("Item " + datum.id.toString() + ", " + datum.title + " missing time.");
+	else {
+		/*Fix nulls in participant descriptions if necessary.
+		var people = datum.people;
+		for (var p=0; p<people.length; p++) {
+			if (people[p].description == null)
+				people[p].description = "";
+		}*/
+
+		var result = {"id": datum.id.toString(),
 								"title": datum.title,
 								"themes": datum.themes,
-								"desc": datum.desc,
+								"desc": datum.desc || "",
 								"format": datum.format,
 								"datetime": datum.datetime,
 								"date": datum.date,
@@ -19,7 +31,8 @@ for (var d=0; d<data.length; d++) {
 								"loc": [datum.loc[0]],
 								"tags": datum.tags,
 								"people": datum.people};
-	out.push(result);
+		out.push(result);
+	}
 }
 //console.log(out);
 fs.writeFile('program.js', "var program = " + JSON.stringify(out) + ";", function (err) {
